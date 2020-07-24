5397d510c5e08c155510993c44c49376080f4e64
zbarovsky <zachary.barovsky@gmail.com>
2020-07-24 13:13:40 -0700

    commit before second half of day

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+
+// initialize Express server
+const app = express()
+
+// require router
+// const users = require('./routes/api/users')
+
+// middleware to allow to CORS requests
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    next()
+})
+
+// bodyParser middleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// config our db
+const db = process.env.MONGOD_URI
+
+// connect to mongodb (using Atlas)
+mongoose.connect(db)
+    .then((() => console.log('MongoDB connected... 🥭')))
+    .catch(err => console.log(err))
+// test routing
+
+// passport middleware
+
+// passport JWT token set/config
+
+// setup our routes
+
+// start our server
+app.listen(process.env.PORT || 5000, () => console.log(`Server is running on port ${process.env.PORT} and things are looking good 😃`))
